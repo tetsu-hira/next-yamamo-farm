67dc73d33e670d625326940b0f61d9a1087b3d89
tetsu.hira <svc.snowdrop@gmail.com>
2022-02-14 09:17:34 +0900

    開発環境構築用

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,10 +3,11 @@
 // import '../styles/globals.css'
 // 追記
 import 'tailwindcss/tailwind.css';
+import { AnimatePresence } from 'framer-motion';
 import type { AppProps } from 'next/app';
 import Layout from '../components/layout';
-import 'swiper/css/bundle';
-import { AnimatePresence } from 'framer-motion';
+// import 'swiper/css/bundle';
+import 'swiper/swiper-bundle.min.css';
 
 function MyApp({ Component, pageProps, router }: AppProps) {
   return (
