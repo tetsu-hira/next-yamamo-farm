1f5a36015a78c81b342f03c27431c632cf8311a8
tetsu.hira <svc.snowdrop@gmail.com>
2022-02-17 10:26:47 +0900

    エラー出たので一旦push

--- src/pages/test.tsx
+++ src/pages/test.tsx
@@ -1,16 +1,20 @@
 import { getApp, FirebaseApp } from 'firebase/app';
 import type { NextPage } from 'next';
+import { SignInForm } from '../components/SignInForm';
 
 import '../lib/init'; // Initialize FirebaseApp
 
 const Test: NextPage = () => {
   const app: FirebaseApp = getApp();
   return (
-    <ul className='pt-36'>
-      <li>name = {app.name}</li>
-      <li>appId = {app.options.appId}</li>
-      <li>apiKey = {app.options.apiKey}</li>
-    </ul>
+    <>
+      <ul className='pt-36'>
+        <li>name = {app.name}</li>
+        <li>appId = {app.options.appId}</li>
+        <li>apiKey = {app.options.apiKey}</li>
+      </ul>
+      <SignInForm />
+    </>
   );
 };
 
